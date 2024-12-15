7bdc62994bd5091b1f13afb0a22c0fab50383cf4
julyevelyn <evelynjuly66@gmail.com>
2024-12-15 16:57:18 +0800

    guagua

--- final-project-main/server.js
+++ final-project-main/server.js
@@ -3,10 +3,33 @@
 const express = require("express");
 const server = express();
 const port = 3000;
+
+var DB = require("nedb-promises");
+var drawDB = DB.create(__dirname + "/draw.db");
 // const cors = require("cors");
 
 // server.use(cors()); // 啟用跨域訪問，讓前端可以請求 API
-server.use(express.static(__dirname + "/GG")); // 提供靜態檔案服務
+server.use(express.static(__dirname + "/GG"));  //提供靜態檔案服務
+
+draw.insert([
+    {title: "天秤宮",description: "高中時畫的作品，第一次嘗試複雜的背景繪製",image: "../img/天秤宮.jpg",},
+    {title: "Maria",description: "最近很喜歡的遊戲角色，又颯又美",image: "/final-project-main/img/maria.JPG",},
+    {title: "Mercy", description: "有一款叫overwatch的遊戲裡，我最喜歡的角色",image: "/final-project-main/img/天使.JPG"},
+    {title: "龍年賀圖",description: "第一次挑戰畫寫實的龍，花了很多時間研究跟摸索",image: "/final-project-main/img/龍.JPG"},
+    {title: "風信與慕情",description: "這是我很喜歡的一對小說角色",image: "/final-project-main/img/風情.JPG"},
+    {title: "兵長", description: "之前很喜歡的一個動漫角色，我花了很多時間研究金屬的刻痕要怎麼繪製",image: "/final-project-main/img/兵長賀圖.jpg"}
+])
+
+server.get("/draw", (req,res)=>{
+    //DB
+    drawDB.find({}).then(results=>{
+      if(results != null){
+           res.send(results);
+      }else{
+          res.send("Error!");
+      }
+    })
+})
 
 // 啟動伺服器
 server.listen(port, () => {
